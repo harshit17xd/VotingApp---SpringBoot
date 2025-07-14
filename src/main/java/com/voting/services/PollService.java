ad07037c96dc9ac1adf916871e7eb26e00f951b8
Harshit Sharma <harshitsharmab17@gmail.com>
2025-07-14 12:16:24 +0530

    created fronted with angular(service,model)

--- src/main/java/com/voting/services/PollService.java
+++ src/main/java/com/voting/services/PollService.java
@@ -3,8 +3,6 @@ package com.voting.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.voting.model.OptionVote;
